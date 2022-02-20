fef795445d52ef796bf9452f81595808236e69b3
Calrissian <va2005art@gmail.com>
2022-02-20 10:53:35 +0300

    Fix CharacterIMG

--- Bot/LogicUI.py
+++ Bot/LogicUI.py
@@ -1,5 +1,4 @@
 from enum import Enum
-
 from BotLogicModule.BotLogic import Logic
 from BotUIDrawer.main import Application
 
@@ -16,7 +15,7 @@ class LogicUI:
     def welcome(self):
         self.drawer.set_text("Здравствуйте, я бот секретарь. Я помогу вам узнать про школу №15. Давайте же начнем. "
                              "Введите /start")
-        self.drawer.set_image(CharacterIMG.WELCOME.src())
+        self.drawer.set_image(CharacterIMG.WELCOME.src)
 
     def click_button_search(self, event):
         text = self.drawer.get_text_user_input()
@@ -24,28 +23,28 @@ class LogicUI:
         if text == "/start":
             variants = self.logic.get_variants_now_level()
             self.drawer.show_choose_variants(variants)
-            self.drawer.set_image(CharacterIMG.DEFAULT.src())
+            self.drawer.set_image(CharacterIMG.DEFAULT.src)
             return
 
         if text.isnumeric() and (self.now_choose_variants is not None and text in self.now_choose_variants):
             return
 
         self.drawer.set_text("Я не понял. Повторите.")
-        self.drawer.set_image(CharacterIMG.ANSWER.src())
+        self.drawer.set_image(CharacterIMG.ANSWER.src)
 
     def run_app(self):
         self.drawer.run_app()
 
 
 class CharacterIMG(Enum):
-    PATH = "media_files/"
     WELCOME = "face2"
     DEFAULT = "face1"
     ANSWER = "face3"
     THING = "face2"
 
-    def __init__(self, src):
-        self.src = src
+    def __init__(self, path):
+        self.path = path
 
+    @property
     def src(self) -> str:
-        return self.PATH.value() + self.src
+        return "media_files/" + self.path
