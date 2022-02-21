36820040726d3f0e82e319e642edfe5d0133951b
danchil2288 <danil.zinovev.00@inbox.ru>
2022-02-21 12:13:26 +0300

    Horizontal scrollbar added

--- Bot/BotUIDrawer/main.py
+++ Bot/BotUIDrawer/main.py
@@ -9,7 +9,7 @@ class Application:
     def __init__(self):
         self.root = Tk()
         self.root.title('Бот секретарь школы №15 С УИОП г. Электросталь')
-        self.root.geometry('700x500')
+        self.root.geometry('820x550')
         self.root.wm_iconbitmap(bitmap=str(Path(pathlib.Path.cwd(), "media_files", "icon.ico")))
         self.root.resizable(width=False, height=False)
 
@@ -21,13 +21,17 @@ class Application:
         self.c_image = self.canvas.create_image(0, 0, anchor='nw', image=self.photo)
         self.canvas.pack(side=LEFT, anchor=NW)
 
-        # top frame
+        # info frame
         self.info_frame = Frame(self.root, height=400, padx=15)
         self.info_frame.pack(fill=X, pady=(15, 5))
 
-        # bottom frame
+        # input frame
         self.input_frame = Frame(self.root, height=100, pady=10, padx=5)
         self.input_frame.pack(fill=X)
+
+        # horizontal scroll frame
+        self.hor_scroll_frame = Frame(self.root)
+        self.hor_scroll_frame.pack(pady=(10, 0), padx=(0, 30), fill=X)
 
         self.lbl_name = Label(self.input_frame, text='Что вы хотите узнать', font=12)
         self.lbl_name.pack(side=LEFT)
@@ -41,13 +45,22 @@ class Application:
         self.btn_search = Button(self.input_frame, text='Узнать', width=10)
         self.btn_search.pack(side=LEFT)
 
-        # Скролл фрагмента
+        # вертикальный скролл фрагмента
         self.scrollbar = Scrollbar(self.info_frame)
         self.scrollbar.pack(side=RIGHT, fill=Y)
         self.txt_widget = Text(self.info_frame, yscrollcommand=self.scrollbar.set, font=15, wrap=WORD)
         self.txt_widget.configure(state='disabled')  # для отмены возможности изменения текста
         self.txt_widget.pack(fill='both')
         self.scrollbar.config(command=self.txt_widget.yview)
+
+        # горизонтальный скролл фрагмента
+        self.hor_scrollbar = Scrollbar(self.hor_scroll_frame)
+        self.hor_txt_widget = Text(self.hor_scroll_frame, height=1, font=12, wrap=NONE)
+        self.hor_txt_widget.configure(xscrollcommand=self.hor_scrollbar.set)
+        self.hor_txt_widget.configure(state='normal')  # для отмены возможности изменения текста
+        self.hor_txt_widget.pack(fill='both')
+        self.hor_scrollbar.config(command=self.hor_txt_widget.xview, orient=HORIZONTAL)
+        self.hor_scrollbar.pack(side=BOTTOM, fill=X)
 
     def get_text_user_input(self):
         return self.ent_name.get()
@@ -59,6 +72,12 @@ class Application:
         self.txt_widget.configure(state='normal')  # для возобновления возможности изменения текста
         self.txt_widget.insert('end', text + "\n")
         self.txt_widget.configure(state='disabled')  # для отмены возможности изменения текста
+
+        # для горизонтального скролла
+    def set_text_hor_scrollbar(self, text: str):
+        self.hor_txt_widget.configure(state='normal')  # для возобновления возможности изменения текста
+        self.hor_txt_widget.insert('end', text + "\n")
+        self.hor_txt_widget.configure(state='disabled')  # для отмены возможности изменения текста
 
     def set_new_text_output(self, text: str):
         self.clear_text_output()
